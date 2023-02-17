add6ef01d2852290d6ee6e2c31408e2d5d8033b6
Ramanjs <raman.s2662@gmail.com>
2023-02-17 23:06:37 +0530

    add: few more queries

--- script.sql
+++ script.sql
@@ -10,9 +10,16 @@ GROUP BY customer_id
 ORDER BY COUNT(customer_id) desc;
 
 -- User requests update password feature 
-update customers 
-set login_password = 'newpassword'
+UPDATE customers 
+SET login_password = 'newpassword'
 where customer_id = 100;
+
+-- Search function in website to search for products based on name or color
+select * from shoe
+where
+name like '%air%'
+or
+color like '%re%';
 
 -- Customer wants to see order history
 select customer_id, first_name, orders.order_id, orders.amount, shoe.name, shoe.price
@@ -78,3 +85,59 @@ from category
   right join reviews on (reviews.shoe_id = shoe.shoe_id)
 where category.name = 'Women'
 order by stars desc;
+
+-- Admin wants to see how much a unique product in sold (most popular product)
+select count(shoe.shoe_id), shoe.shoe_id
+from order_shoe
+join shoe on (order_shoe.shoe_id = shoe.shoe_id)
+group by shoe_id
+order by count(shoe.shoe_id) desc;
+
+-- Create a view to filter children's shoes by stars/rating
+create or replace view FilterChildrenShoesOrderByRating as
+select reviews.shoe_id, price, shoe.name, color, weight, quantity, stars
+from category
+  join shoe on (category.category_id = shoe.category_id)
+  right join reviews on (reviews.shoe_id = shoe.shoe_id)
+where category.name = 'Children'
+order by stars desc;
+
+-- Customer wants to filter products by their preferred price range
+select * from shoe
+where price <= 80000 and price >= 60000
+order by price asc;
+
+-- Admin wants to analyse which sellers sell the most products in a month
+select count(order_shoe.shoe_id) as shoes_sold, shoe_seller.seller_id
+from order_shoe
+right join shoe_seller on (shoe_seller.shoe_id = order_shoe.shoe_id)
+group by shoe_seller.seller_id
+order by count(order_shoe.shoe_id) desc;
+
+-- Customer requests for address update
+update customers 
+set city = 'newcity',
+piccode = 'newpin',
+state = 'newstate'
+where customer_id = 100;
+
+-- Admin wants to check rating history for a seller
+select AVG(ratings.stars) as stars, sellers.seller_id
+from ratings
+left join sellers on (ratings.seller_id = sellers.seller_id)
+group by sellers.seller_id
+order by AVG(ratings.stars) desc;
+
+-- Seller wants to see their sold history
+select order_id, shoe_seller.shoe_id
+from order_shoe
+join shoe_seller on (shoe_seller.shoe_id = order_shoe.shoe_id)
+where shoe_seller.seller_id = 438;
+
+-- Seller wants to calculate his gross profit after removing the commision 
+select SUM(shoe.price - (shoe.price * 5 / 100)) as profit, shoe_seller.seller_id as seller_id
+from order_shoe
+join shoe_seller on (order_shoe.shoe_id = shoe_seller.shoe_id)
+left join shoe on (shoe.shoe_id = shoe_seller.shoe_id)
+group by shoe_seller.seller_id
+order by profit desc;
